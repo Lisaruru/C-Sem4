1147bde61c4a1a95bde0bca9a4468d2e5a47a6cd
m.gorshkov <m.gorshkov@skbkontur.ru>
2022-08-21 15:52:17 +0800

    Add exs27

---
+++ Exs27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+using System;
+
+namespace Exs27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int number=Convert.ToInt32(Console.ReadLine());
+            int sum=0;
+            int digit=0;
+
+             while(number>0)
+             {
+                digit=number%10;
+                sum=sum+digit;
+                number=number/10;
+               
+             }
+            Console.WriteLine(sum);
+        }
+    }
+}
