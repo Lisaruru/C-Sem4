7db2efafddbc6a98d563564925bf40460acfe3b3
m.gorshkov <m.gorshkov@skbkontur.ru>
2022-08-21 16:17:51 +0800

    Add exs29

---
+++ Exs29/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+using System;
+
+namespace Exs29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[ ]arr=new int[8];
+            void RandomArray(int[]array)
+            {
+                for(int i=0;i<8;i++)
+                {
+                    Random rnd=new Random();
+                    int value=rnd.Next(0,100);
+                    arr[i]=value;
+                    Console.Write($"{arr[i]}  ");
+                }
+          
+            }
+                    RandomArray(arr);
+        }
+    }
+}
